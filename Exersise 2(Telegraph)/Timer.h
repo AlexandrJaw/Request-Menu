d363b3429343ef915ed8d6295b839575cbf8d18d
Александр Яворский <s1a9s9h9a@gmail.com>
2023-05-10 01:34:20 +0300

    Допилил таймер

---
+++ Exersise 2(Telegraph)/Timer.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <time.h>
+using namespace std;
+
+using millis = clock_t;
+using second = clock_t;
+using hour = clock_t;
+using minutes = clock_t;
+
+class Timer
+{
+private:
+	millis start = clock();
+public:
+
+	inline void reset(void)
+	{
+		start = clock();
+	}
+
+	inline millis getElapsedTime(void)
+	{
+		return clock() - start;
+	}
+
+	inline second getSecond(millis mil)
+	{
+		return mil / 1000;
+	}
+
+	inline minutes getMinutes(millis mil)
+	{
+		return getSecond(mil) / 60;
+	}
+
+	inline hour getHour(millis mil)
+	{
+		return getMinutes(mil) / 60;
+	}
+};
