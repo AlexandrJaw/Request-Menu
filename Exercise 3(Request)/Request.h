8c1cb28260110de0a58194b56cb62af19745c0d4
Александр Яворский <s1a9s9h9a@gmail.com>
2023-05-12 02:59:11 +0300

    Добавил вывод заявки

--- Exercise 3(Request)/Request.h
+++ Exercise 3(Request)/Request.h
@@ -3,12 +3,24 @@
 #include <chrono>
 #include "Date.h"
 #include "FulName.h"
+#include <iostream>
 
 class Request
 {
+private:
 	std::string m_destination;
-	int m_flightNumber;
-	FullName name;
-	Date date;
+	int m_flyghtNumber;
+	FullName m_name;
+	Date m_date;
+public:
+	Request(const std::string &dest, const int flyNum, const FullName &name, const Date &date)
+		: m_destination(dest), m_date(date), m_flyghtNumber(flyNum), m_name(name) {}
+
+	friend std::ostream& operator<<(std::ostream &out, const Request &req)
+	{
+		out << req.m_destination << '\t' << req.m_flyghtNumber << '\t'
+			<< req.m_name << '\t' << req.m_date;
+		return out;
+	}
 };
 
