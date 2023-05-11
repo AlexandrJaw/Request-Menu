a96b05c058351466b57acf7f2eeb7341fce6c7a2
Александр Яворский <s1a9s9h9a@gmail.com>
2023-05-11 03:13:58 +0300

    Удалил структуру и добавил одноимённый класс для имён

---
+++ Exercise 3(Request)/FulName.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+class FullName
+{
+private:
+	std::string m_firstName;
+	std::string m_seondName;
+	std::string m_lastName;
+public:
+	
+};
+
--- Exercise 3(Request)/Request.h
+++ Exercise 3(Request)/Request.h
@@ -1,23 +1,14 @@
 #pragma once
 #include <string>
 #include <chrono>
-
-struct FullName;
+#include "Date.h"
+#include "FulName.h"
 
 class Request
 {
 	std::string m_destination;
 	int m_flightNumber;
 	FullName name;
-	
+	Date date;
 };
 
-struct FullName
-{
-private:
-	std::string m_firstName;
-	std::string m_seondName;
-	std::string m_lastName;
-public:
-
-};
