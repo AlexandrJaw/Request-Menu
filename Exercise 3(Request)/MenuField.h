88fe888cb8d44225ec92bc525669e19cbf5c5172
Александр Яворский <s1a9s9h9a@gmail.com>
2023-05-13 06:00:10 +0300

    Удалил класс меню добавления полей. Клас поле меню больше не абстрактный.

--- Exercise 3(Request)/MenuField.h
+++ Exercise 3(Request)/MenuField.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <string>
 
-class AMenuField
+class MenuField
 {
 private:
 	std::string m_name;
 public:
-	AMenuField(std::string &name) : m_name(name) {}
+	MenuField(std::string &name) : m_name(name) {}
 
 	virtual void execute() = 0;
 };
