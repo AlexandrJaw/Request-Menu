e59de9980e35a5aa528068247a2362f6ee73249a
Александр Яворский <s1a9s9h9a@gmail.com>
2023-05-13 06:16:45 +0300

    Сделал класс полей

--- Exercise 3(Request)/MenuField.h
+++ Exercise 3(Request)/MenuField.h
@@ -1,13 +1,19 @@
 #pragma once
 #include <string>
+#include <functional>
 
 class MenuField
 {
 private:
 	std::string m_name;
+	std::function<void()> m_action;
 public:
-	MenuField(std::string &name) : m_name(name) {}
+	MenuField(std::string &name, std::function<void()>& fcn) 
+		: m_name(name), m_action(fcn) {}
 
-	virtual void execute() = 0;
+	void setName(const std::string &name) { m_name = name; }
+	void setAction(const std::function<void()>& action) { m_action = action; }
+	const std::string& getName() const { return m_name; }
+	void execute() const { m_action(); }
 };
 
