0b7c5e5e1c5660074455d10c86b8c8560658691b
Александр Яворский <s1a9s9h9a@gmail.com>
2023-05-25 00:24:46 +0300

    Исправил баг с вводом

--- Exercise 3(Request)/ReqList.cpp
+++ Exercise 3(Request)/ReqList.cpp
@@ -21,47 +21,59 @@ bool ReqList::addRequest() // Метод добавления новой заявки на авиабилет
 		m_req = std::make_unique<Request>(); 
 		isFirstAddThisRequest = false;
 	}
-	else if(!isLastRoundOfInput)
-		std::cout << "INVALID INPUT" << std::endl;
-	
+	else if (!isLastRoundOfInput)
+	{
+		cout << "INVALID INPUT" << std::endl << std::endl;
+		cout << "Вы ввели: " << std::endl;
+	}
+	else
+		cout << "Вы ввели: " << std::endl;
+
 	//Цикл проверки ввода
 	if(!isInputsCorect(m_flags) || isLastRoundOfInput)
 	{
 		if (!m_flags[0])
 		{
 			cout << "Введите пункт назначения: ";
-			cin >> dest;
+			if (cin.peek() == '\n')
+				cin.ignore();
+			std::getline(cin, dest);
 			m_flags[0] = m_req->setDestination(std::move(dest));
 			if (!m_flags[0]) return false; //Если ввод был некоректным, тогда нужно повторить цикл ввода с выводом при этом всего введённого до этого
 		}
 		else
-			std::cout << "Вы ввели пункт назначения: " << m_req->getDestination() << std::endl;
+			std::cout << " " << "1. Пункт назначения: " << m_req->getDestination() << std::endl;
 
 		if (!m_flags[1])
 		{
 			cout << "Введите номер рейса в формате 'AAA000': ";
-			cin >> numFly;
+			if (cin.peek() == '\n')
+				cin.ignore();
+			std::getline(cin, numFly);
 			m_flags[1] = m_req->setFlyghtNumber(std::move(numFly));
 			if (!m_flags[1]) return false;
 		}
 		else
-			std::cout << "Вы ввели номер рейса: " << m_req->getFlyghtNumber() << std::endl;
+			std::cout << " " << "2. Номер рейса: " << m_req->getFlyghtNumber() << std::endl;
 
 		if (!m_flags[2])
 		{
 			cout << "Введите ваши имя фамилию через пробел: ";
-			cin.ignore();
+			if(cin.peek() == '\n')
+				cin.ignore();
 			std::getline(cin, name);
 			m_flags[2] = m_req->setName(std::move(name));
 			if (!m_flags[2]) return false;
 		}
 		else
-			std::cout << "Вы ввели ваши имя/фамилию: " << m_req->getName() << std::endl;
+			std::cout << " " << "3. Ваши имя/фамилию: " << m_req->getName() << std::endl;
 
 		if (!m_flags[3])
 		{
 			cout << "Введите желаемую дату вылета в формате 'дд/мм/гг': ";
-			cin >> sdate;
+			if (cin.peek() == '\n')
+				cin.ignore();
+			std::getline(cin, sdate);
 			m_flags[3] = m_req->setDate(std::move(sdate));
 			if (!m_flags[3]) return false;
 
@@ -69,7 +81,7 @@ bool ReqList::addRequest() // Метод добавления новой заявки на авиабилет
 			return false; // Команда необходима для того что бы вывести в конце результат ввода пользователя 
 		}
 		else
-			std::cout << "Вы ввели желаемую дату вылета: " << m_req->getDate() << std::endl;
+			std::cout << " " << "4. Желаемую дату вылета: " << m_req->getDate() << std::endl;
 	}
 	system("pause");
 
