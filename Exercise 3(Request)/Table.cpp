c5ffb7911c0745b81a2b2fd4252e4fd7911c98aa
Александр Яворский <s1a9s9h9a@gmail.com>
2023-05-25 04:05:22 +0300

    Продолжаю реализовывать класс таблицы

--- Exercise 3(Request)/Table.cpp
+++ Exercise 3(Request)/Table.cpp
@@ -1,1 +1,29 @@
 #include "Table.h"
+
+Table& Table::operator<<(std::string &&cell)
+{
+	if(m_table.size() == 0)
+		m_table.push_back(std::vector<std::string>());
+	m_table.at(m_activeLine).push_back(cell);
+	return *this;
+}
+
+Table& Table::nextLine()
+{
+	m_table.push_back(std::vector<std::string>());
+	++m_activeLine;
+	return *this;
+}
+
+std::ostream& operator<<(std::ostream &out, const Table &table)
+{
+	auto copyTable = table.m_table;
+
+	int maxLengthStr = 0;
+	for(int i(0); i < copyTable.size(); ++i)
+		for (int j(0); j < copyTable.at(i).size(); ++j)
+		{
+			if(maxLengthStr < copyTable[i][j].size())
+				maxLengthStr = copyTable[i][j].size();   //TODO: Доделать выравнивание таблицы
+		}
+}
--- Exercise 3(Request)/Table.h
+++ Exercise 3(Request)/Table.h
@@ -2,14 +2,23 @@
 #include <vector>
 #include <iomanip>
 #include <iostream>
+#include <string>
 
 class Table
 {
 private:
 	std::vector<std::vector<std::string>> m_table;
+	int m_activeLine;
 public:
+	Table() : m_activeLine(0) {}
 	Table(Table &table) { m_table = table.m_table; }
 	Table(Table &&table) { m_table = std::move(table.m_table); }
+
+	Table& nextLine();
+
+	Table& operator<<(std::string &&cell);
+	Table& operator<<(Table &table) {}
+	friend std::ostream& operator<<(std::ostream &out, const Table &table);
 
 	Table& operator=(Table &&table)
 	{
